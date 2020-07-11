21a02bd37f3e61094c4cf3dada29b14df72533e3
spyland8k <omerpinar93@gmail.com>
2020-07-11 20:19:18 +0300

    redirecting coming message from webhook

--- bot.js
+++ bot.js
@@ -4,36 +4,88 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const webhook = require('webhook-discord');
 const client = new Discord.Client();
-
-function hookedMsg(){
-
-}
+const boosterCut = 20;
+// webhook messages drops here (channel id)
+const hookForm = "731543421340221521";
+// routing to booster channel
+const webhookToChannelId = "731232365388759111";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getChannelbyId(channelId) {
+    return client.channels.cache.get(channelId);
+}
+
+function modifyEmbed(message){
+    let msg = (await message.fetch());
+    let embed = new Discord.MessageEmbed(msg.embeds[0]);
+
+    /*// What are those coming from webhook
+    console.log(embed.title + " embed message");
+    let i = 0;
+    embed.fields.forEach(element => {
+        console.log(i + "- " + element.name + " " + element.value);
+        i++;
+    });*/
+
+    // Modify hooked message
+    let newEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(embed.title)
+        //.setDescription('Some description here')
+        .setThumbnail('https://bnetcmsus-a.akamaihd.net/cms/template_resource/fh/FHSCSCG9CXOC1462229977849.png')
+        .addFields(
+            // Discord Tag
+            //{ name: embed.fields[0].name, value: embed.fields[0].value, inline: true },
+            // Faction
+            { name: embed.fields[1].name, value: embed.fields[1].value, inline: true },
+            // Keystone Level
+            { name: embed.fields[2].name, value: embed.fields[2].value, inline: true },
+            // Dungeon Key
+            { name: embed.fields[3].name, value: embed.fields[3].value, inline: true },
+            // Armor Stacked
+            { name: embed.fields[4].name, value: embed.fields[4].value, inline: true },
+            // Number Of Boost
+            { name: embed.fields[5].name, value: embed.fields[5].value, inline: true },
+            // Boost Price
+            { name: embed.fields[6].name, value: embed.fields[6].value, inline: true },
+            // TODO Booster Cut
+            { name: "Booster Cut", value: toString(parseFloat(embed.fields[6].value) / boosterCut), inline: true },
+            // Realm
+            { name: embed.fields[7].name, value: embed.fields[7].value, inline: true },
+            // Char to Whisper /w %nick
+            { name: embed.fields[8].name, value: embed.fields[8].value, inline: true },
+            // Note
+            { name: embed.fields[11].name, value: embed.fields[11].value, inline: true }
+        )
+        //.addField('Inline field title', 'Some value here', true)
+        //.setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('BoostId: ' + message.id, 'https://bnetcmsus-a.akamaihd.net/cms/template_resource/fh/FHSCSCG9CXOC1462229977849.png');
+    return newEmbed
+}
+
 client.on('message', async message => {
+
     const filter = (reaction, user) => {
         return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
     };
+
     // is that message comes from webhook
     // 731232365388759111
     if(message.webhookID){
-        var channel = message.channel;
         //get webhook post
-        var msg = (await message.fetch());
-        console.log(msg + "fetched message");
-
-        var embed = new Discord.MessageEmbed(msg.embeds[0]);
-        console.log(embed + "embed message");
-
-        var exEmbed = new Discord.MessageEmbed(embed)
-            .setTitle("TESTER")
-            .setDescription("TEST description");
-
-        channel.send(exEmbed);
-        console.log(exEmbed + " embed[" + "0" + "] name");
+        var newEmbed = modifyEmbed(message);
+        // get channel
+        var messageToChannel = getChannelbyId(webhookToChannelId);
+        // Send, Created new modified message to webhookToChannelId
+        messageToChannel.send(newEmbed);
 
         msg.react('✅').then(() => message.react('❌'));
 
